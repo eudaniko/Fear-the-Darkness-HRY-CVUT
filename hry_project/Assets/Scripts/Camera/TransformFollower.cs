5650296bedc01fb83ce76b054dab4597a27d971a
yakov <yakov.balashov@gmail.com>
2023-10-15 15:34:45 +0200

    Add PlayerMovement, PlayerHealth, TransformFollower

---
+++ hry_project/Assets/Scripts/Camera/TransformFollower.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace FtDCode.Camera
+{
+    public class TransformFollower : MonoBehaviour
+    {
+        // Follows player ignoring horizontal movement, might be replaced by the "Cinemachine"
+        [SerializeField] private Transform followedTransform;
+        private float _offset;
+        private Vector3 _position;
+
+        private void Awake()
+        {
+            _offset = followedTransform.position.y;
+            _position = transform.position;
+        }
+
+        private void Update()
+        {
+            transform.position = new Vector3(_position.x, followedTransform.position.y - _offset, _position.z);
+        }
+    }
+}
---
+++ hry_project/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace FtDCode.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float horizontalSpeed;
+        [SerializeField] private float verticalSpeed;
+        private Rigidbody2D _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            MoveRigidbody();
+        }
+
+        private void MoveRigidbody()
+        {
+            _rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * horizontalSpeed, verticalSpeed);
+        }
+
+        public void ChangeVerticalSpeed(float deltaSpeed)
+        {
+            verticalSpeed += deltaSpeed;
+        }
+    }
+}
