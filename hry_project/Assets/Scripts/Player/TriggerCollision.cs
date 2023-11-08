69be7f311d39fea73f3df449cf58ad82733abbd6
eudaniko <139865394+eudaniko@users.noreply.github.com>
2023-11-08 21:33:11 +0100

    Add physics Material for Character

--- hry_project/Assets/Scripts/Player/TriggerCollision.cs
+++ hry_project/Assets/Scripts/Player/TriggerCollision.cs
@@ -1,3 +1,4 @@
+using System;
 using FtDCode.Core;
 using UnityEngine;
 
@@ -5,23 +6,23 @@ namespace FtDCode.Player
 {
     public class TriggerCollision : MonoBehaviour
     {
-         [SerializeField] private ScenesManager gameManager;
-        
-        private Animator _animator; 
-        
+        [SerializeField] private ScenesManager gameManager;
+
+        private Animator _animator;
+
         private PlayerMovement _player;
         [SerializeField] private float deltaVerticalSlow;
         [SerializeField] private float deltaHorizontalSlow;
 
         private PlayerHealth _playerHealth;
         [SerializeField] private float deltaWater;
-        
+
         private static readonly string SlowingObstacle = "SpiderWeb";
         private static readonly string DeathWall = "DeathWall";
         private static readonly string DeathTriangle = "DeathTriangle";
         private static readonly string Water = "Water";
-        
-        
+
+
         private void Awake()
         {
             _player = GetComponent<PlayerMovement>();
@@ -33,15 +34,16 @@ namespace FtDCode.Player
         {
             if (other.gameObject.CompareTag(SlowingObstacle))
             {
-                _animator.SetBool("isSlowed",true);
+                _animator.SetBool("isSlowed", true);
             }
+
             if (other.gameObject.CompareTag(Water))
             {
                 _playerHealth.ChangeHpValue(deltaWater);
                 other.GetComponent<CircleCollider2D>().enabled = false;
             }
         }
-        
+
 
         private void OnTriggerStay2D(Collider2D other)
         {
@@ -55,15 +57,15 @@ namespace FtDCode.Player
                 _player.ChangeHorizontalSpeed(deltaHorizontalSlow);
             }
         }
-        
+
         private void OnTriggerExit2D(Collider2D other)
         {
             if (other.gameObject.CompareTag(SlowingObstacle))
             {
                 _player.ResetSpeed();
-                _animator.SetBool("isSlowed",false);
+                _animator.SetBool("isSlowed", false);
             }
+
         }
-        
     }
 }
