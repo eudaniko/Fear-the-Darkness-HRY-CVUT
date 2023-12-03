b96c9d522506ddee6b463e2942486041aecbcaef
eudaniko <139865394+eudaniko@users.noreply.github.com>
2023-12-03 12:11:21 +0100

    Improve Boss Movement

--- hry_project/Assets/Scripts/Boss/BossMovement.cs
+++ hry_project/Assets/Scripts/Boss/BossMovement.cs
@@ -1,80 +1,109 @@
 using UnityEngine;
-using System;
+
 
 namespace FtDCode.Boss
 {
     public class BossMovement : MonoBehaviour
     {
-        [SerializeField] private float defaultVerticalSpeed;
-        private float verticalSpeed;
-        private float defaultBossDistance;
-        private float lastDistance;
-        private float actualDistance;
+        [SerializeField] private float defaltDistance;
+        [SerializeField] private float defaltVertivalSpeed;
+        private float _deltaVerticalSpeed ;
+        [SerializeField] private float slowingBossDelta;
         private Rigidbody2D _rigidbody;
-        private Transform bossPosition;
-        [SerializeField] private float needTimeForResetBoss = 6f;
-        private float _deltaTime;
+        private Transform _bossPosition;
         [SerializeField] private Transform characterPosition;
+        [SerializeField] private float needTimeForResetBoss;
+        private float _deltaTime;
+        private BossState _currentState;
+
+        private enum BossState { Approaching, Retreating, Stable }
 
         private void Awake()
         {
-            verticalSpeed = defaultVerticalSpeed;
+            _deltaVerticalSpeed = defaltVertivalSpeed;
             _rigidbody = GetComponent<Rigidbody2D>();
-            bossPosition = GetComponent<Transform>();
-            defaultBossDistance = Mathf.Abs(bossPosition.position.y - characterPosition.position.y);
+            _bossPosition = transform;
+            _currentState = BossState.Stable;
         }
 
         private void FixedUpdate()
         {
             MoveRigidbody();
-            actualDistance = Mathf.Round(Mathf.Abs(bossPosition.position.y - characterPosition.position.y));
-            if (actualDistance < defaultBossDistance && lastDistance == actualDistance)
-            {
-                lastDistance = actualDistance;
-                _deltaTime += Time.deltaTime;
-                if (_deltaTime >= needTimeForResetBoss)
-                {
-                    ChangeBossSpeed(-0.02f);
-                }
-            }
-            else if(lastDistance != actualDistance)
-            {
-                ResetBossSpeed();
-                lastDistance = actualDistance;
-                _deltaTime = 0;
-            }
-            else if (actualDistance > defaultBossDistance && lastDistance == actualDistance)
-            {
-                lastDistance = actualDistance;
-                _deltaTime += Time.deltaTime;
-                if (_deltaTime >= needTimeForResetBoss)
-                {
-                    ChangeBossSpeed(0.02f);
-                }
-            }
+            CheckDistanceAndUpdateState();
         }
 
         private void MoveRigidbody()
         {
-            _rigidbody.velocity = new Vector2(0, verticalSpeed);
+            _rigidbody.velocity = new Vector2(0, _deltaVerticalSpeed);
+        }
+
+        private void CheckDistanceAndUpdateState()
+        {
+            float actualDistance = Mathf.Abs(_bossPosition.position.y - characterPosition.position.y);
+            UnityEngine.Debug.Log(actualDistance);
+            UnityEngine.Debug.Log(_currentState);
+            switch (_currentState)
+            {
+                case BossState.Stable:
+                    if (actualDistance > defaltDistance)
+                    {
+                        UpdateState(BossState.Stable);
+                    }
+                    else if (actualDistance < defaltDistance)
+                    {
+                        UpdateState(BossState.Retreating);
+                    }
+                    break;
+                case BossState.Retreating:
+                    if (_deltaTime >= needTimeForResetBoss)
+                    {
+                        UnityEngine.Debug.Log("Change speed");
+                        AdjustBossSpeed();
+                        _deltaTime = 0;
+                    }
+                    else if (actualDistance > defaltDistance)
+                    {
+                        UpdateState(BossState.Stable);
+                        ResetBossSpeed();
+                    }
+
+                    UnityEngine.Debug.Log(_deltaTime);
+                    _deltaTime += Time.deltaTime;
+                    break;
+            }
+        }
+
+        private void UpdateState(BossState newState)
+        {
+            _currentState = newState;
+            _deltaTime = 0; // Reset timer when state changes
+        }
+
+        private void AdjustBossSpeed()
+        {
+            if (_currentState == BossState.Approaching)
+            {
+                ChangeBossSpeed(+slowingBossDelta);
+            }
+            else if (_currentState == BossState.Retreating)
+            {
+                ChangeBossSpeed(-slowingBossDelta);
+            }
         }
 
         private void ChangeBossSpeed(float deltaSpeed)
         {
-            if (verticalSpeed >= 0)
-            {
-                verticalSpeed += deltaSpeed;
-            }
+            _deltaVerticalSpeed += deltaSpeed;
         }
 
-        private void ResetBossSpeed()
+        public void ResetBossSpeed()
         {
-            verticalSpeed = defaultVerticalSpeed;
+            _deltaVerticalSpeed = defaltVertivalSpeed;
         }
 
         public void SlowDownBoss(float slowedSpeed)
         {
-            verticalSpeed = slowedSpeed;
+            _deltaVerticalSpeed = slowedSpeed;
         }
     }
 }
