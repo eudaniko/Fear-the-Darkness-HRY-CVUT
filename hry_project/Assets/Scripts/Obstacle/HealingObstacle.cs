49113f22c9a786403a5a5277965ebcbd268b8560
eudaniko <139865394+eudaniko@users.noreply.github.com>
2023-12-18 19:57:57 +0100

    Update Boss, HealingObstacle

--- hry_project/Assets/Scripts/Obstacle/HealingObstacle.cs
+++ hry_project/Assets/Scripts/Obstacle/HealingObstacle.cs
@@ -8,14 +8,17 @@ namespace FtDCode.Obstacle
     {
         private Animator _animator;
         private static readonly int Active = Animator.StringToHash("Active");
+        private AudioSource _audioSource;
 
         private void Start()
         {
             _animator = GetComponent<Animator>();
+            _audioSource = GetComponent<AudioSource>();
         }
         
         public void Interact(GameObject player)
         {
+            _audioSource.Play();
             player.GetComponent<PlayerHealth>().SetHpToMax();
             _animator.SetBool(Active, false);
 
